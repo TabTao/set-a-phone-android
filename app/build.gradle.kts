e62dc34ec850f29aeb5d5600931d9f5adbc4d86e
eitao@Dell <eitao@126.com>
2026-08-18 12:32:50 +0800

    fix(android) 修复构建并递增版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.setaphone.remote"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.4.4"
+        versionCode = 9
+        versionName = "0.4.5"
     }
 
     compileOptions {
